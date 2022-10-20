5a4506e1cad2c2f22ca850a4e1eb681f2bf30789
UP210042 <up210042@alumnos.upa.edu.mx>
2022-10-20 13:33:32 -0500

    Trabajos

---
+++ U3/E02_Biseccion.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double funEcuacion1(double x)
+{ 
+    double y;            //x2^-8x+15         //pow= elevar un numero a la potencia que le elijas
+    return (pow(x,2)-8*x+15);
+}
+
+int main()
+{
+    double x1=4;
+    double x2=7;
+    double xm;
+    double Es=.001; //el numero de ceros indica el numero de digitod //Error estandar
+    double Er=abs(x2-x1); //error relativo//abs= valor absoluto
+    double r=funEcuacion1(0);
+    
+    int i=1;
+    while(Er > Es )
+    {
+        xm=(x1+x2)/2;
+        if(funEcuacion1(x1)* funEcuacion1(xm)<0)
+        {
+            x2=xm;
+        }
+        else
+        { x1=xm; }
+        Er=abs(x2-x1);
+        i=i+1;
+    }
+    cout<<"i= "<< i <<"raiz= "<<xm<<endl;
+    cout<<r<<endl;
+    return 0;
+}
+
